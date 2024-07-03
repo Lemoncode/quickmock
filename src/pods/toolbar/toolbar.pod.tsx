350696129fc5274c53f697a99b750b8a844e11c0
LAPTOP-G2U17NSB\duy_9 <lourdesrsdp@gmail.com>
2024-07-03 14:26:54 +0200

    Implementing main layout using CSS Grid in `main.module.css`

--- src/pods/toolbar/toolbar.pod.tsx
+++ src/pods/toolbar/toolbar.pod.tsx
@@ -1,7 +1,7 @@
 export const ToolbarPod = () => {
   return (
-    <>
+    <div className="toolbar-grid">
       <h1>Hello Quickmock from Toolbar Pod !!!</h1>
-    </>
+    </div>
   );
 };
--- src/scenes/main.scene.tsx
+++ src/scenes/main.scene.tsx
@@ -1,3 +1,4 @@
+import classes from "./main.module.css";
 import {
   CanvasPod,
   ToolbarPod,
@@ -7,11 +8,11 @@ import {
 
 export const MainScene = () => {
   return (
-    <>
-      <ToolbarPod />
-      <ContainerGalleryPod />
-      <ComponentGalleryPod />
-      <CanvasPod />
-    </>
+    <div className={classes.layout} >
+      <div className={classes.toolbar}><ToolbarPod /></div>
+      <div className={classes.containerGallery}><ContainerGalleryPod /></div>
+      <div className={classes.componentGallery}><ComponentGalleryPod /></div>
+      <div className={classes.canvas}><CanvasPod /></div>
+    </div>
   );
 };
