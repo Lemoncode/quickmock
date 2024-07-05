b886103b40e0fc7943b6f73fa96a1839035dbcff
Fran Lopez <franciscolopto@gmail.com>
2024-07-05 17:20:03 +0200

    config husky and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+   browser: true,
+   es2021: true,
+  },
+  extends: [
+   'eslint:recommended',
+   'plugin:react/recommended',
+   'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+   ecmaFeatures: {
+     jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+   },
+ }; 
