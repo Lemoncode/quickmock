c9bf0241863e1ab4637152b832cb70efe288b6f3
IonutGabi <birsanionut@gmail.com>
2024-07-10 14:05:55 +0200

    Fix checkbox.svg type in src/pods/component-gallery/component-gallery-data/index.ts

--- src/pods/component-gallery/component-gallery-data/index.ts
+++ src/pods/component-gallery/component-gallery-data/index.ts
@@ -1,8 +1,8 @@
 import { ItemInfo } from '@/common/components/gallery/components/model';
 
 export const mockWidgetCollection: ItemInfo[] = [
-  { thumbnailSrc: '/widgets/button.svg', type: 'input' },
+  { thumbnailSrc: '/widgets/button.svg', type: 'button' },
   { thumbnailSrc: '/widgets/input.svg', type: 'input' },
-  { thumbnailSrc: '/widgets/combobox.svg', type: 'input' },
-  { thumbnailSrc: '/widgets/checkbox.svg', type: 'input' },
+  { thumbnailSrc: '/widgets/combobox.svg', type: 'combobox' },
+  { thumbnailSrc: '/widgets/checkbox.svg', type: 'checkbox' },
 ];
