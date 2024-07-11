c693ed67ffee65f5ea683bc811d1a047cb928bd8
Braulio <braulio.diez@gmail.com>
2024-07-11 12:52:02 +0200

    basic implementation

--- src/pods/canvas/canvas.pod.tsx
+++ src/pods/canvas/canvas.pod.tsx
@@ -1,18 +1,21 @@
 import classes from './canvas.pod.module.css';
-import { ComboBoxShape } from '@/common/components/front-components';
-import { createRef, useState } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
 import { Layer, Stage, Transformer } from 'react-konva';
 import { ShapeModel, createShape } from './canvas.model';
 import { useSelection } from './use-selection.hook';
 import Konva from 'konva';
 import { useTransform } from './use-transform.hook';
-import {
-  fitSizeToShapeSizeRestrictions,
-  getShapeSizeRestrictions,
-} from './canvas.util';
-import { Box } from 'konva/lib/shapes/Transformer';
-import { InputShape } from '@/common/components/front-components/input-shape';
 import { renderShapeComponent } from './shape-renderer';
+import {
+  dropTargetForElements,
+  monitorForElements,
+} from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import invariant from 'tiny-invariant';
+import {
+  convertFromDivElementCoordsToKonvaCoords,
+  extractScreenCoordinatesFromPragmaticLocation,
+  portScreenPositionToDivCoordinates,
+} from './canvas.util';
 
 export const CanvasPod = () => {
   const [shapes, setShapes] = useState<ShapeModel[]>([
@@ -32,6 +35,71 @@ export const CanvasPod = () => {
     selectedShapeType,
   } = useSelection(shapes);
 
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+  const dropRef = useRef(null);
+  const stageRef = useRef<Konva.Stage>(null);
+
+  useEffect(() => {
+    const el = dropRef.current;
+
+    invariant(el);
+
+    return dropTargetForElements({
+      element: el,
+      getData: () => ({ destination: 'canvas' }),
+      onDragEnter: () => setIsDraggedOver(true),
+      onDragLeave: () => setIsDraggedOver(false),
+      onDrop: () => setIsDraggedOver(false),
+    });
+  });
+
+  useEffect(() => {
+    return monitorForElements({
+      onDrop({ source, location }) {
+        const destination = location.current.dropTargets[0];
+        invariant(destination);
+
+        const type = source.data.type;
+        const screenPosition =
+          extractScreenCoordinatesFromPragmaticLocation(location);
+
+        let positionX = 0;
+        let positionY = 0;
+        if (screenPosition) {
+          invariant(dropRef.current);
+          const { x: divRelativeX, y: divRelativeY } =
+            portScreenPositionToDivCoordinates(
+              dropRef.current as HTMLDivElement,
+              screenPosition
+            );
+
+          invariant(stageRef.current);
+          const stage = stageRef.current;
+          const konvaCoord = convertFromDivElementCoordsToKonvaCoords(
+            stage,
+            screenPosition,
+            {
+              x: divRelativeX,
+              y: divRelativeY,
+            }
+          );
+
+          positionX = konvaCoord.x;
+          positionY = konvaCoord.y;
+        }
+
+        setShapes(shapes => [
+          ...shapes,
+          createShape(
+            { x: positionX, y: positionY },
+            { width: 200, height: 50 }, // TODO: each shape should provide it's own size
+            type as any
+          ),
+        ]);
+      },
+    });
+  }, []);
+
   const { handleTransform, handleTransformerBoundBoxFunc } = useTransform(
     setShapes,
     {
@@ -49,14 +117,22 @@ export const CanvasPod = () => {
       );
     };
 
+  {
+    /* TODO: add other animation for isDraggerOver */
+  }
   return (
-    <div className={classes.canvas}>
+    <div
+      className={classes.canvas}
+      ref={dropRef}
+      style={{ opacity: isDraggedOver ? 0.5 : 1 }}
+    >
       {/*TODO: move size to canvas provider?*/}
       <Stage
         width={3000}
         height={3000}
         onMouseDown={handleClearSelection}
         onTouchStart={handleClearSelection}
+        ref={stageRef}
       >
         <Layer>
           {
--- src/pods/canvas/canvas.util.ts
+++ src/pods/canvas/canvas.util.ts
@@ -1,10 +1,12 @@
 import { ShapeSizeRestrictions, ShapeType } from '@/core/model';
-import { Size } from './canvas.model';
+import { Coord, Size } from './canvas.model';
 import {
   getComboBoxShapeSizeRestrictions,
   getInputShapeSizeRestrictions,
   getToggleSwitchShapeSizeRestrictions,
 } from '@/common/components/front-components';
+import { DragLocationHistory } from '@atlaskit/pragmatic-drag-and-drop/dist/types/internal-types';
+import { Stage } from 'konva/lib/Stage';
 
 // TODO Add Unit tests, issue: #45
 export const fitSizeToShapeSizeRestrictions = (
@@ -51,4 +53,46 @@ export const getShapeSizeRestrictions = (type: ShapeType | null) => {
     default:
       return defaultShapeSizeRestrictions;
   }
+};
+
+// TODO Add unit tests to this functions
+export const extractScreenCoordinatesFromPragmaticLocation = (
+  location: DragLocationHistory
+) => {
+  const pragmaticDropInfo = location.current.input;
+  return {
+    x: pragmaticDropInfo.clientX,
+    y: pragmaticDropInfo.clientY,
+  };
+};
+
+export const portScreenPositionToDivCoordinates = (
+  divElement: HTMLDivElement,
+  screenPosition: Coord
+): Coord => {
+  const canvasRect = divElement.getBoundingClientRect();
+  const x = screenPosition.x - canvasRect.left;
+  const y = screenPosition.y - canvasRect.top;
+
+  return { x, y };
+};
+
+export const convertFromDivElementCoordsToKonvaCoords = (
+  stage: Stage,
+  screenPosition: Coord,
+  relativeDivPosition: Coord
+): Coord => {
+  stage.setPointersPositions([screenPosition.x, screenPosition.y]);
+  const result: Coord = { x: 0, y: 0 };
+
+  const pointerPosition = stage.getPointerPosition();
+  if (pointerPosition) {
+    const scaleX = stage.scaleX();
+    const scaleY = stage.scaleY();
+
+    result.x = (relativeDivPosition.x - stage.x()) / scaleX;
+    result.y = (relativeDivPosition.y - stage.y()) / scaleY;
+  }
+
+  return result;
 };
