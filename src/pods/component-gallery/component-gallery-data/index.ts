0abbff4a9053b868c46a8634d8a99d4ed59ad554
Raquel <rtoscano@gmail.com>
2024-07-25 19:06:09 +0200

    timepicker widget added

--- src/pods/component-gallery/component-gallery-data/index.ts
+++ src/pods/component-gallery/component-gallery-data/index.ts
@@ -8,4 +8,5 @@ export const mockWidgetCollection: ItemInfo[] = [
   { thumbnailSrc: '/widgets/checkbox.svg', type: 'checkbox' },
   { thumbnailSrc: '/widgets/toggleswitch.svg', type: 'toggleswitch' },
   { thumbnailSrc: '/widgets/datepicker.svg', type: 'datepickerinput' },
+  { thumbnailSrc: '/widgets/timepicker.svg', type: 'timepickerinput' },
 ];
