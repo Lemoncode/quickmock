31b69fd34c6c6d2bbcdd62de3ee4a3870c1f3d18
Raquel <rtoscano@gmail.com>
2024-08-03 21:00:18 +0200

    added hook to manage states with interceptor

---
+++ src/core/providers/canvas/canvas.hook.tsx
@@ -0,0 +1,38 @@
+import React, { Dispatch, SetStateAction } from 'react';
+
+/*
+  This is a wrapper for useState
+
+  In this case we want to execute some function whenever the state change
+
+  For instance Undo / Redo functionallity.
+
+  Whenever you do a setState you want to store it in the queue of changes
+  so you can undo it later, instead of having to remember to call storeInUndoQueue
+  you can just call _setState_ and it will do it for you.
+*/
+export function useStateWithInterceptor<S>(
+  initialState: S | (() => S),
+  schemaInterceptorFn: (schema: S) => void
+): [S, Dispatch<SetStateAction<S>>, Dispatch<SetStateAction<S>>] {
+  const [shapes, setInternalShapes] = React.useState<S>(initialState);
+
+  const setShapes = (newShapes: React.SetStateAction<S>): void => {
+    // If newShapes is a function, use it to calculate the new state based on the current state
+    // Otherwise, use newShapes directly
+    const updatedShapes =
+      newShapes instanceof Function ? newShapes(shapes) : newShapes;
+
+    schemaInterceptorFn(updatedShapes);
+
+    return setInternalShapes(newShapes);
+  };
+
+  const setShapesSkipInterceptor = (
+    newShapes: React.SetStateAction<S>
+  ): void => {
+    return setInternalShapes(newShapes);
+  };
+
+  return [shapes, setShapes, setShapesSkipInterceptor];
+}
