40c47f0eca798ff0f2471ba61fd16da79fbe537f
Raquel <rtoscano@gmail.com>
2024-08-05 10:07:15 +0200

    added call to reDo and debuginfo

--- src/pods/toolbar/components/redo-button/redo-button.tsx
+++ src/pods/toolbar/components/redo-button/redo-button.tsx
@@ -1,10 +1,16 @@
 import { RedoIcon } from '@/common/components/icons/redo-icon.component';
 import ToolbarButton from '../toolbar-button/toolbar-button';
 import classes from '@/pods/toolbar/toolbar.pod.module.css';
+import { useCanvasContext } from '@/core/providers';
 
 export const RedoButton = () => {
+  const { doRedo, shapes } = useCanvasContext();
   const handleClick = () => {
     console.log('Redo');
+
+    //TODO: REMOVE THIS
+    console.log('shapes: ', shapes);
+    doRedo();
   };
 
   return (
