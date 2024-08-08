fc9b9c1ba25652c5861563bf41b9404be6b294f5
LAPTOP-G2U17NSB\duy_9 <lourdesrsdp@gmail.com>
2024-08-08 15:09:46 +0200

    Adding Basic shape gallery Created frist basic-shape: rectangular

---
+++ src/common/components/front-basic-sapes/rectangle-basic-shape.tsx
@@ -0,0 +1,47 @@
+import { ShapeSizeRestrictions } from '@/core/model';
+import { forwardRef } from 'react';
+import { ShapeProps } from '../front-components/shape.model';
+import { fitSizeToShapeSizeRestrictions } from '@/common/utils/shapes/shape-restrictions';
+import { Group, Rect } from 'react-konva';
+
+const rectangleShapeRestrictions: ShapeSizeRestrictions = {
+  minWidth: 60,
+  minHeight: 45,
+  maxWidth: -1,
+  maxHeight: -1,
+  defaultWidth: 170,
+  defaultHeight: 45,
+};
+
+export const getRectangleShapeSizeRestrictions = (): ShapeSizeRestrictions =>
+  rectangleShapeRestrictions;
+
+export const RectangleShape = forwardRef<any, ShapeProps>(
+  ({ x, y, width, height, id, onSelected, text, ...shapeProps }, ref) => {
+    const { width: restrictedWidth, height: restrictedHeight } =
+      fitSizeToShapeSizeRestrictions(rectangleShapeRestrictions, width, height);
+
+    return (
+      <Group
+        x={x}
+        y={y}
+        ref={ref}
+        width={restrictedWidth}
+        height={restrictedHeight}
+        {...shapeProps}
+        onClick={() => onSelected(id, 'rectangle')}
+      >
+        <Rect
+          x={0}
+          y={0}
+          width={restrictedWidth}
+          height={restrictedHeight}
+          strokeWidth={2}
+          stroke="black"
+          cornerRadius={5}
+          fill="lightgrey"
+        />
+      </Group>
+    );
+  }
+);
