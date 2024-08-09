43aae5dd3014c831df5bd854a95624e90531026c
Rodrigo <devrodrigolec@gmail.com>
2024-08-09 20:24:27 +0200

    Modal created and closeWindow working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { MainScene } from "./scenes/main.scene";
+import { ModalDialogComponent } from './common/components/modal-dialog/modal-dialog.component';
+import { MainScene } from './scenes/main.scene';
 
 function App() {
   return (
     <>
       <MainScene />
+      <ModalDialogComponent />
     </>
   );
 }
---
+++ src/common/components/modal-dialog/modal-dialog.component.tsx
@@ -0,0 +1,41 @@
+import classes from './modal-dialog.component.module.css';
+import React from 'react';
+
+interface Props {
+  title: string;
+  isOpen: boolean;
+}
+
+export const ModalDialogComponent: React.FC<Props> = (props: Props) => {
+  const { title } = props;
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  React.useEffect(() => {
+    console.log(isOpen);
+  }, [isOpen]);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    isOpen && (
+      <div className={classes.dialogModal}>
+        <div className={classes.mainDiv}>
+          <div className={classes.xCloseModalDiv}>
+            <h2>About us</h2>
+            <button
+              className={classes.xCloseModal}
+              onClick={handleClick}
+            ></button>
+          </div>
+          <div>
+            <h1>{title ?? 'Dialog Modal Title'}</h1>
+            <h2>Prueba</h2>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    )
+  );
+};
