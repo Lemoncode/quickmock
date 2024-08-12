3ee3339374524c4a4657e89196b9e6442c6a061e
Ivanruii <ivr.644@gmail.com>
2024-08-12 13:29:37 -0400

    Made checkbox editable

--- src/pods/canvas/canvas.model.ts
+++ src/pods/canvas/canvas.model.ts
@@ -131,6 +131,7 @@ const doesShapeAllowInlineEdition = (shapeType: ShapeType): boolean => {
     case 'button':
     case 'textarea':
     case 'accordion':
+    case 'checkbox':
       return true;
     default:
       return false;
@@ -151,6 +152,8 @@ const generateDefaultTextValue = (shapeType: ShapeType): string | undefined => {
       return 'Your text here...';
     case 'accordion':
       return '[*]Section A\nSection B';
+    case 'checkbox':
+      return 'Check me!';
     default:
       return undefined;
   }
