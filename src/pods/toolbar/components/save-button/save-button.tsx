514cd24722e2c52c3cd82e7437250ed37cad3184
Pablo <p.reinaldo90@gmail.com>
2024-08-12 21:14:22 +0200

    save file button

--- src/pods/toolbar/components/save-button/save-button.tsx
+++ src/pods/toolbar/components/save-button/save-button.tsx
@@ -1,18 +1,46 @@
 import { SaveIcon } from '@/common/components/icons/save-icon.component';
 import classes from '@/pods/toolbar/toolbar.pod.module.css';
 import { ToolbarButton } from '../toolbar-button';
+import { shapes } from 'konva/lib/Shape';
+
+const DEFAULT_FILE_NAME = 'quick-mock';
+const DEFAULT_EXTENSION_DESCRIPTION = 'Quick Mock';
+const DEFAULT_FILE_EXTENSION = 'png'; //sobreentiendo que es png
 
 export const SaveButton = () => {
-  const handleClick = () => {
-    console.log('Save');
-  };
 
+  const myObject = { name: 'hello' };
+  const content = JSON.stringify(myObject); 
+
+
+
+  const saveFile = async (openedFilename: string) => {
+    const filename = openedFilename !== '' ? openedFilename : DEFAULT_FILE_NAME;
+
+    const blob = new Blob([content], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    if (window.showDirectoryPicker === undefined) {  //no me funciona showdirectorypicker
+      downloadFile(filename, content, 'application/json'); //ubicación downloadFile?
+    } else {
+      const savedFilename = await saveFileModern( //ubicación saveFileModern?
+        {
+          filename,
+          extension: DEFAULT_FILE_EXTENSION,
+          description: DEFAULT_EXTENSION_DESCRIPTION,
+        },
+        content
+      );
+    }
+    URL.revokeObjectURL(url);
+  };
   return (
     <ToolbarButton
-      onClick={handleClick}
-      className={classes.button}
       icon={<SaveIcon />}
-      label="Save"
+      label={'Save'}
+      onClick={() => saveFile('')} //de donde saco el filename
+      className={`${classes.button} hide-mobile`}
     />
   );
 };
+
