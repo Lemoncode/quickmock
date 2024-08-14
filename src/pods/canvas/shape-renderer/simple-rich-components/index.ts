bc40d977d4866de9585361708ea9177ca45bab34
Braulio <braulio.diez@gmail.com>
2024-08-14 11:58:18 +0200

    updated barrel

--- src/pods/canvas/shape-renderer/simple-rich-components/index.ts
+++ src/pods/canvas/shape-renderer/simple-rich-components/index.ts
@@ -3,3 +3,4 @@ export * from './accordion.renderer';
 export * from './pie-chart.renderer';
 export * from './horizontal-menu.renderer';
 export * from './map-chart.renderer';
+export * from './breadcrumb.renderer';
