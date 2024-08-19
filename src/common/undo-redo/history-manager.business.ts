afa340d884f3d462a33d2e076c33f1b0cef5624d
Raquel <rtoscano@gmail.com>
2024-08-19 21:38:27 +0200

    Made addSnapshotToHistory idempotent for several rendering calls

--- src/common/undo-redo/history-manager.business.ts
+++ src/common/undo-redo/history-manager.business.ts
@@ -1,22 +1,28 @@
 // TODO Add Unit tests to this Undo/Redo helpers
 // #162
 // https://github.com/Lemoncode/mongo-modeler/issues/162
+import isEqual from 'lodash/isEqual';
+
 export function addSnapshotToHistory<T>(
   history: T[],
   newSnapshot: T,
   currentIndex: number,
   maxHistoryLength: number
 ): [T[], number] {
-  let newHistory = history.slice(0, currentIndex + 1);
-  newHistory.push(newSnapshot);
+  if (isEqual(newSnapshot, history[currentIndex]))
+    return [history, currentIndex];
+  else {
+    let newHistory = history.slice(0, currentIndex + 1);
+    newHistory.push(newSnapshot);
 
-  if (newHistory.length > maxHistoryLength) {
-    newHistory = newHistory.slice(1);
-  } else {
-    currentIndex++;
+    if (newHistory.length > maxHistoryLength) {
+      newHistory = newHistory.slice(1);
+    } else {
+      currentIndex++;
+    }
+
+    return [newHistory, currentIndex];
   }
-
-  return [newHistory, currentIndex];
 }
 
 export function canUndo(currentIndex: number): boolean {
