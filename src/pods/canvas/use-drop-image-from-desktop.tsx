80bc24097fcc61fc00ce5017a5ad53ea7c302987
Braulio <braulio.diez@gmail.com>
2024-08-19 19:14:13 +0200

    refactor image drop

--- src/pods/canvas/use-drop-image-from-desktop.tsx
+++ src/pods/canvas/use-drop-image-from-desktop.tsx
@@ -3,20 +3,20 @@ import { useCanvasContext } from '@/core/providers';
 export const useDropImageFromDesktop = () => {
   const { addNewShape } = useCanvasContext();
 
+  // TODO: move this to utils / business
+  // Try to unify the code to check if the file is an image (on DragOver and Drop)
   const isDropImageFile = (e: React.DragEvent<HTMLDivElement>) => {
-    const file = e.dataTransfer.files[0];
-
-    return file?.type?.startsWith('image/');
+    return (
+      e.dataTransfer.items.length > 0 &&
+      e.dataTransfer.items[0].kind === 'file' &&
+      e.dataTransfer.items[0].type.startsWith('image/')
+    );
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     // TODO: Refactor this, try a way to unifiy or get some common way
     // to handle drag over and drop
-    if (
-      e.dataTransfer.items.length > 0 &&
-      e.dataTransfer.items[0].kind === 'file' &&
-      e.dataTransfer.items[0].type.startsWith('image/')
-    ) {
+    if (isDropImageFile(e)) {
       e.preventDefault();
       e.stopPropagation();
     }
