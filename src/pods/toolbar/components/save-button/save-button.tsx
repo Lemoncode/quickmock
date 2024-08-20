7c701a631564599429ca5c12aeddf285faefc056
Pablo <p.reinaldo90@gmail.com>
2024-08-20 22:36:08 +0200

    Added OldBrowserDownloadFile

--- src/pods/toolbar/components/save-button/save-button.tsx
+++ src/pods/toolbar/components/save-button/save-button.tsx
@@ -3,37 +3,50 @@ import classes from '@/pods/toolbar/toolbar.pod.module.css';
 import { ToolbarButton } from '../toolbar-button';
 import { useCanvasContext } from '@/core/providers';
 
-const DEFAULT_FILE_NAME = 'document';
 
+const DEFAULT_FILE_NAME = 'mymockui';
 
 export const SaveButton: React.FC = () => {
-  const { fileName, setFileName } = useCanvasContext();
-  const saveFile = (openedFileName: string) => {
-    const newfileName = openedFileName !== '' ? openedFileName : DEFAULT_FILE_NAME;
-    //const url = URL.createObjectURL(blob);
-    //const blob = new Blob([jsonString], { type: 'application/json' });
-    /*console.log('Save');
-    const myObject = { name: 'hello' };
-    const jsonString = JSON.stringify(myObject);
+
+  const { shapes } = useCanvasContext();
+
+
+
+  const serializeShapes = (): string => {
+
+    return JSON.stringify(shapes);
+  };
+
+  const OldBrowsersDownloadFile = (filename: string) => {
+
+    const blob = new Blob([serializeShapes()], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `${fileName}.json`;
+    a.download = `${filename}.qm`;
     a.click();
-    URL.revokeObjectURL(url);*/
+    URL.revokeObjectURL(url);
+
+  }
+  const saveFile = () => {
+
+    const filename = DEFAULT_FILE_NAME;
     if (window.showDirectoryPicker === undefined) {
-      console.log("download file")
+      OldBrowsersDownloadFile(filename);
     } else {
       console.log("save file")
     }
-  };
-
+  }
+  
+  
   return (
     <ToolbarButton
-      onClick={saveFile}
-      className={classes.button}
-      icon={<SaveIcon />}
-      label="Save"
+    onClick={saveFile}
+    className={classes.button}
+    icon={<SaveIcon />}
+    label="Save"
     />
   );
+  
 };
 
