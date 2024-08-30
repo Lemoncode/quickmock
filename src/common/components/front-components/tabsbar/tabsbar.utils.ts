86ddead679876d229f5200f6a03c45657a9d3ab5
Fran Lopez <franciscolopto@gmail.com>
2024-08-30 11:48:44 +0200

    add comment

--- src/common/components/front-components/tabsbar/tabsbar.utils.ts
+++ src/common/components/front-components/tabsbar/tabsbar.utils.ts
@@ -10,7 +10,7 @@ export const parseCSVHeader = (headerRow: string): Header[] => {
   return headerRow.split(',').map(header => {
     const trimmedHeader = header.trim();
 
-    // Captura el texto sin el sorting ni espacios
+    // Captures text without sorting and spaces
     const textWithoutAlignment = trimmedHeader
       .replace(/(?:\s+\^?\s*v?|\s+v?\s*\^?)$/, '')
       .trim();
