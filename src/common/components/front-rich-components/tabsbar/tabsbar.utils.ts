4d19c59a597815c52ed45856f4d9421174bb8fe3
Fran Lopez <franciscolopto@gmail.com>
2024-09-05 11:08:03 +0200

    add avoid empty tabs after a ,

--- src/common/components/front-rich-components/tabsbar/tabsbar.utils.ts
+++ src/common/components/front-rich-components/tabsbar/tabsbar.utils.ts
@@ -7,15 +7,16 @@ export const splitCSVIntoRows = (csvContent: string): string[] => {
 };
 
 export const parseCSVHeader = (headerRow: string): Header[] => {
-  return headerRow.split(',').map(header => {
-    const trimmedHeader = header.trim();
-
-    // Captures text without sorting and spaces
-    const textWithoutAlignment = trimmedHeader
-      .replace(/(?:\s+\^?\s*v?|\s+v?\s*\^?)$/, '')
-      .trim();
-    return {
-      text: textWithoutAlignment,
-    };
-  });
+  return headerRow
+    .split(',')
+    .map(header => header.trim())
+    .filter(header => header.length > 0)
+    .map(header => {
+      const textWithoutAlignment = header
+        .replace(/(?:\s+\^?\s*v?|\s+v?\s*\^?)$/, '')
+        .trim();
+      return {
+        text: textWithoutAlignment,
+      };
+    });
 };
