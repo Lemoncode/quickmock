62b7cc3c4dbb37cbd437b429634ccc92f123de08
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2024-09-11 18:08:29 +0200

    fix component

--- src/common/components/front-rich-components/audio-player.tsx
+++ src/common/components/front-rich-components/audio-player.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from 'react';
 import { Group, Line, Rect } from 'react-konva';
-import { ShapeProps } from '../front-components/shape.model'; // Ajusta la ruta según tu estructura
+import { ShapeProps } from '../front-components/shape.model';
 import { useShapeComponentSelection } from '../shapes/use-shape-selection.hook';
 import { ShapeSizeRestrictions, ShapeType } from '@/core/model';
 import { fitSizeToShapeSizeRestrictions } from '@/common/utils/shapes/shape-restrictions';
@@ -20,7 +20,7 @@ export const getAudioPlayerShapeSizeRestrictions = (): ShapeSizeRestrictions =>
 
 const shapeType: ShapeType = 'audioPlayer';
 const arrowWidth = 20;
-const volumeWidth = 20;
+const volumeWidth = 25;
 
 export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
   const { x, y, width, height, id, onSelected, ...shapeProps } = props;
@@ -39,6 +39,9 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
     BASIC_SHAPE.DEFAULT_PADDING -
     (arrowWidth + BASIC_SHAPE.DEFAULT_PADDING) * 4;
 
+  const margin = 10;
+  const controlBarHeight = 30;
+
   const { handleSelection } = useShapeComponentSelection(props, shapeType);
 
   return (
@@ -51,14 +54,12 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
       height={restrictedHeight}
       {...shapeProps}
     >
-      {/* Fondo de la barra de scroll */}
       <Rect
         x={arrowWidth}
         y={0}
         width={restrictedWidth - arrowWidth * 2}
         height={restrictedHeight}
-        fill="#D0D0D0"
-        stroke="#000000"
+        fill="white"
         strokeWidth={1}
       />
 
@@ -68,15 +69,21 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
         y={0}
         width={arrowWidth}
         height={restrictedHeight}
-        fill="#E0E0E0"
-        stroke="#A0A0A0"
         strokeWidth={1}
       />
 
       <Line
+        x={-37}
+        y={-30}
+        points={[65, 45, 65, 60]}
+        stroke="black"
+        strokeWidth={2}
+      />
+
+      <Line
         x={4}
-        y={restrictedHeight / 2}
-        points={[8, -4, 2, 0, 8, 4]}
+        y={restrictedHeight / 2.3}
+        points={[16, -8, 4, 0, 16, 8]}
         fill="black"
         closed={true}
       />
@@ -87,15 +94,13 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
         y={0}
         width={arrowWidth}
         height={restrictedHeight}
-        fill="#E0E0E0"
-        stroke="#A0A0A0"
         strokeWidth={1}
       />
 
       <Line
         x={arrowWidth + BASIC_SHAPE.DEFAULT_PADDING + 6}
-        y={restrictedHeight / 2}
-        points={[2, -4, 8, 0, 2, 4]}
+        y={restrictedHeight / 2.3}
+        points={[4, -8, 16, 0, 4, 8]}
         fill="black"
         closed={true}
       />
@@ -105,15 +110,21 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
         y={0}
         width={arrowWidth}
         height={restrictedHeight}
-        fill="#E0E0E0"
-        stroke="#A0A0A0"
         strokeWidth={1}
       />
 
       <Line
+        x={0}
+        y={-30}
+        points={[65, 45, 65, 60]}
+        stroke="black"
+        strokeWidth={2}
+      />
+
+      <Line
         x={(arrowWidth + BASIC_SHAPE.DEFAULT_PADDING + 6) * 2}
-        y={restrictedHeight / 2}
-        points={[2, -4, 8, 0, 2, 4]}
+        y={restrictedHeight / 2.3}
+        points={[4, -8, 16, 0, 4, 8]}
         fill="black"
         closed={true}
       />
@@ -122,7 +133,7 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
         x={(arrowWidth + BASIC_SHAPE.DEFAULT_PADDING) * 3}
         y={restrictedHeight / 4}
         width={calculateWidth}
-        height={restrictedHeight / 2}
+        height={restrictedHeight / 3}
         stroke="black"
         strokeWidth={1}
         closed={true}
@@ -131,12 +142,56 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
         x={(arrowWidth + BASIC_SHAPE.DEFAULT_PADDING) * 3}
         y={restrictedHeight / 4}
         width={calculateWidth / 2}
-        height={restrictedHeight / 2}
-        stroke="black"
+        height={restrictedHeight / 3}
+        stroke="white"
         fill="black"
         strokeWidth={1}
         closed={true}
       />
+
+      {/* Control de volumen mejorado */}
+      <Group>
+        {/* Altavoz */}
+        <Line
+          x={arrowWidth + BASIC_SHAPE.DEFAULT_PADDING + 15}
+          y={-10}
+          points={[
+            restrictedWidth - 2 * margin - 70,
+            restrictedHeight - controlBarHeight + 5,
+            restrictedWidth - 2 * margin - 65,
+            restrictedHeight - controlBarHeight + 5,
+            restrictedWidth - 2 * margin - 60,
+            restrictedHeight - controlBarHeight,
+            restrictedWidth - 2 * margin - 60,
+            restrictedHeight - controlBarHeight + 20,
+            restrictedWidth - 2 * margin - 65,
+            restrictedHeight - controlBarHeight + 15,
+            restrictedWidth - 2 * margin - 70,
+            restrictedHeight - controlBarHeight + 15,
+            restrictedWidth - 2 * margin - 70,
+            restrictedHeight - controlBarHeight + 5,
+          ]}
+          stroke="black"
+          fill="black"
+          strokeWidth={2}
+          closed={true}
+        />
+        <Line
+          x={arrowWidth + BASIC_SHAPE.DEFAULT_PADDING + 15}
+          y={-10}
+          points={[
+            restrictedWidth - 2 * margin - 60,
+            restrictedHeight - controlBarHeight + 5,
+            restrictedWidth - 2 * margin - 55,
+            restrictedHeight - controlBarHeight + 10,
+            restrictedWidth - 2 * margin - 60,
+            restrictedHeight - controlBarHeight + 15,
+          ]}
+          stroke="black"
+          fill="black"
+          strokeWidth={2}
+        />
+      </Group>
     </Group>
   );
 });
