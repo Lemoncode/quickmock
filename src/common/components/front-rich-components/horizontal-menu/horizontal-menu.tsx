3b8367799c2295d5cb647021a65e713ebcde9fce
Fran Lopez <franciscolopto@gmail.com>
2024-09-17 13:11:59 +0200

    add horizontal menu active selected item and refactor

--- src/common/components/front-rich-components/horizontal-menu/horizontal-menu.tsx
+++ src/common/components/front-rich-components/horizontal-menu/horizontal-menu.tsx
@@ -1,15 +1,18 @@
 import { Group, Rect, Text } from 'react-konva';
 import { ShapeSizeRestrictions, ShapeType } from '@/core/model';
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef } from 'react';
 import { ShapeProps } from '../../front-components/shape.model';
 import { fitSizeToShapeSizeRestrictions } from '@/common/utils/shapes/shape-restrictions';
 import { BASIC_SHAPE } from '../../front-components/shape.const';
 import { useShapeComponentSelection } from '../../shapes/use-shape-selection.hook';
-import { mapHorizontalMenuTextToItems } from './hozontal-menu.business';
 import { useShapeProps } from '../../shapes/use-shape-props.hook';
+import {
+  extractCSVHeaders,
+  splitCSVContentIntoRows,
+} from '@/common/utils/active-element-selector.utils';
 
 const horizontalMenuShapeSizeRestrictions: ShapeSizeRestrictions = {
-  minWidth: 75,
+  minWidth: 200,
   minHeight: 25,
   maxWidth: -1,
   maxHeight: 100,
@@ -35,36 +38,17 @@ export const HorizontalMenu = forwardRef<any, ShapeProps>((props, ref) => {
     ...shapeProps
   } = props;
 
-  const [selectedItem, setSelectedItem] = useState<number | null>(null);
-  const [items, setItems] = useState<string[]>([
-    '[*]Home, About, Services, Contact',
-  ]);
-  const handleClick = (itemIndex: number) => {
-    setSelectedItem(itemIndex);
-    onSelected(id, 'horizontal-menu', true);
-  };
+  const csvData = splitCSVContentIntoRows(text);
+  const headers = extractCSVHeaders(csvData[0]);
+  const itemLabels = headers.map(header => header.text);
 
-  useEffect(() => {
-    if (typeof text === 'string') {
-      const { items, selectedItemIndex } = mapHorizontalMenuTextToItems(text);
-      setItems(items);
-      setSelectedItem(selectedItemIndex);
-    } else {
-      setItems([]);
-    }
-  }, [text]);
+  const numberOfItems = itemLabels.length;
+  const itemSpacing = 10;
 
-  const numberOfItems = items.length;
-  const minItemWidth = 100;
-  const itemSpacing = 20;
-  const totalWidth = Math.max(
-    minItemWidth * numberOfItems + itemSpacing * (numberOfItems + 1),
-    width
-  );
   const { width: restrictedWidth, height: restrictedHeight } =
     fitSizeToShapeSizeRestrictions(
       horizontalMenuShapeSizeRestrictions,
-      totalWidth,
+      width,
       height
     );
   const totalMargins = restrictedWidth - itemSpacing * (numberOfItems + 1);
@@ -76,7 +60,10 @@ export const HorizontalMenu = forwardRef<any, ShapeProps>((props, ref) => {
     otherProps,
     BASIC_SHAPE
   );
+
   const itemVerticalPadding = 4;
+
+  const activeSelected = otherProps?.activeElement ?? 0;
 
   return (
     <Group
@@ -100,21 +87,19 @@ export const HorizontalMenu = forwardRef<any, ShapeProps>((props, ref) => {
         cornerRadius={borderRadius}
       />
 
-      {items.map((e: string, index: number) => (
-        <Group key={index} onClick={() => handleClick(index)}>
+      {itemLabels.map((header, index) => (
+        <Group key={index}>
           <Rect
             x={itemSpacing * (index + 1) + itemWidth * index}
             y={itemVerticalPadding}
             width={itemWidth}
             height={restrictedHeight - 2 * itemVerticalPadding}
-            fill={selectedItem === index ? 'lightblue' : fill}
-            stroke={selectedItem === index ? 'skyblue' : 'transparent'}
-            strokeWidth={selectedItem === index ? 1 : 0}
+            fill={index === activeSelected ? 'lightblue' : fill}
           />
           <Text
             x={itemSpacing * (index + 1) + itemWidth * index}
             y={restrictedHeight / 2 - 8}
-            text={e}
+            text={header}
             fontFamily="Arial"
             fontSize={16}
             fill={textColor}
