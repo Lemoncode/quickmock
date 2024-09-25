bb88006c54e6af7e6cfb2d3a5673bf8553f1da2d
Jorge Miranda <mirandajorge97@gmail.com>
2024-09-25 20:01:13 +0200

    Refactor isDropImageFile

--- src/pods/canvas/canvas.util.spec.ts
+++ src/pods/canvas/canvas.util.spec.ts
@@ -1,3 +1,78 @@
+import { isDropImageFile } from './canvas.util';
+
 describe('', () => {
-  it('should return True when there is an image in the event');
+  it('should return True when there is an image in the item when dropped', () => {
+    //Arrange
+    const mockImagefile = {
+      kind: 'file',
+      type: 'image/png',
+    };
+
+    const mockDatatransfer = {
+      DataTransfer: {
+        items: [mockImagefile],
+      },
+    } as unknown as React.DragEvent<HTMLDivElement>;
+
+    //Act
+    isDropImageFile(mockDatatransfer);
+
+    //Assert
+    expect(isDropImageFile(mockDatatransfer)).toBe(true);
+  });
+
+  it('should return False when there is not an image in the item when dropped', () => {
+    //Arrange
+    const mockImagefile = {
+      kind: 'file',
+      type: 'text',
+    };
+
+    const mockDatatransfer = {
+      DataTransfer: {
+        items: [mockImagefile],
+      },
+    } as unknown as React.DragEvent<HTMLDivElement>;
+
+    //Act
+    isDropImageFile(mockDatatransfer);
+
+    //Assert
+    expect(isDropImageFile(mockDatatransfer)).toBe(false);
+  });
+
+  it('should return False when the kind in the item is not file', () => {
+    //Arrange
+    const mockImagefile = {
+      kind: 'string',
+      type: 'image/png',
+    };
+
+    const mockDatatransfer = {
+      DataTransfer: {
+        items: [mockImagefile],
+      },
+    } as unknown as React.DragEvent<HTMLDivElement>;
+
+    //Act
+    isDropImageFile(mockDatatransfer);
+
+    //Assert
+    expect(isDropImageFile(mockDatatransfer)).toBe(false);
+  });
+
+  it('should return False when no items are dropped', () => {
+    //Arrange
+    const mockDatatransfer = {
+      DataTransfer: {
+        items: [],
+      },
+    } as unknown as React.DragEvent<HTMLDivElement>;
+
+    //Act
+    isDropImageFile(mockDatatransfer);
+
+    //Assert
+    expect(isDropImageFile(mockDatatransfer)).toBe(false);
+  });
 });
