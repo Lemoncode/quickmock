ac6dc0562cca8c9f4d3de591322569812e5be424
Antonio Contreras <antoniocontrerasroman@gmail.com>
2024-10-11 13:49:12 +0200

    work in progress

--- src/common/components/mock-components/front-rich-components/audio-player.tsx
+++ src/common/components/mock-components/front-rich-components/audio-player.tsx
@@ -1,9 +1,9 @@
 import { forwardRef } from 'react';
 import { Line, Rect, Path, Group } from 'react-konva';
 import { ShapeProps } from '../shape.model';
-import { useShapeComponentSelection } from '@/common/components/shapes/use-shape-selection.hook';
 import { ShapeSizeRestrictions, ShapeType } from '@/core/model';
 import { fitSizeToShapeSizeRestrictions } from '@/common/utils/shapes/shape-restrictions';
+import { useGroupShapeProps } from '../mock-components.utils';
 
 const AudioPlayerShapeSizeRestrictions: ShapeSizeRestrictions = {
   minWidth: 280,
@@ -23,14 +23,13 @@ const shapeType: ShapeType = 'audioPlayer';
 export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
   const { x, y, width, height, id, onSelected, ...shapeProps } = props;
 
-  const { width: restrictedWidth, height: restrictedHeight } =
-    fitSizeToShapeSizeRestrictions(
-      AudioPlayerShapeSizeRestrictions,
-      width,
-      height
-    );
+  const restrictedSize = fitSizeToShapeSizeRestrictions(
+    AudioPlayerShapeSizeRestrictions,
+    width,
+    height
+  );
 
-  const { handleSelection } = useShapeComponentSelection(props, shapeType);
+  const { width: restrictedWidth, height: restrictedHeight } = restrictedSize;
 
   // Ratios for the elements in relation to the component
   const ratioX = width / restrictedWidth;
@@ -74,22 +73,16 @@ export const AudioPlayerShape = forwardRef<any, ShapeProps>((props, ref) => {
     30 * ratioY,
   ];
 
+  const commonGroupProps = useGroupShapeProps(
+    props,
+    restrictedSize,
+    shapeType,
+    ref
+  );
+
   return (
-    <Group
-      x={x}
-      y={y}
-      ref={ref}
-      onClick={handleSelection}
-      width={restrictedWidth}
-      height={restrictedHeight}
-      {...shapeProps}
-    >
-      <Rect
-        x={0}
-        y={0}
-        width={restrictedWidth}
-        height={restrictedHeight}
-      ></Rect>
+    <Group {...commonGroupProps} {...shapeProps}>
+      <Rect x={0} y={0} width={restrictedWidth} height={restrictedHeight} />
 
       {/* Back Button */}
       <Line
