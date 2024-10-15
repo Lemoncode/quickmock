24fb7eb22941c2d7eb4b3c6401a1bd1db85c5fa8
Alber EE <122263897+Alber-Writer@users.noreply.github.com>
2024-10-15 11:17:55 +0200

    Add QA test multiple selection unselect

--- e2e/selection/multiple-selection.spec.ts
+++ e2e/selection/multiple-selection.spec.ts
@@ -20,3 +20,29 @@ test('Should perform multiple selection when dragging and dropping over multiple
   const selectedItems = await getTransformer(page);
   expect(selectedItems._nodes.length).toEqual(3);
 });
+
+test('Should deselect all previously selected items when clicking on an empty point on the canvas', async ({
+  page,
+}) => {
+  await page.goto('');
+
+  //Drag and drop component to canvas
+  const componentsAtCanvas = ['Input', 'Input', 'Icon', 'Label'];
+  await addComponentsToCanvas(page, componentsAtCanvas);
+
+  //Click Away
+  await page.mouse.click(800, 130);
+
+  //Perform items selection by drag and drop
+  await dragAndDrop(page, { x: 260, y: 130 }, { x: 1000, y: 550 });
+
+  //Assert
+  const selectedItems = await getTransformer(page);
+  expect(selectedItems._nodes.length).toEqual(3);
+
+  //Click Away
+  await page.mouse.click(800, 130);
+  //Assert
+  const updatedSelectedItems = await getTransformer(page);
+  expect(updatedSelectedItems._nodes.length).toEqual(0);
+});
