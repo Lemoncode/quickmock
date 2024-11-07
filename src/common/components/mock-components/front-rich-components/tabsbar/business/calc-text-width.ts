061d7bf8ffc869de5340c1437cbb3d9e73b6d71e
Alber EE <122263897+Alber-Writer@users.noreply.github.com>
2024-11-07 22:41:08 +0100

    adjust tabsbar text-width methods

--- src/common/components/mock-components/front-rich-components/tabsbar/business/calc-text-width.ts
+++ src/common/components/mock-components/front-rich-components/tabsbar/business/calc-text-width.ts
@@ -3,6 +3,28 @@ export const calcTextWidth = (
   fontSize: number,
   _fontfamily: string
 ) => {
-  const charAverageWidth = fontSize * 0.5;
-  return inputText.length * charAverageWidth + charAverageWidth / 2;
+  const REGX_DEFS = {
+    LOWERCASE_BIG: /[mwoq]/,
+    LOWERCASE: /[a-z0-9]/,
+    UPPERCASE_BIG: /[MWOQ]/,
+    UPPERCASE: /[A-Z]/,
+    MARKS: /[.,;:!?'"(){}\[\]\-]/,
+    SPACE: /\s/,
+    DEFAULT: 'default',
+  };
+
+  const calcLength = [...inputText].reduce((sum, current) => {
+    const testChar = (regx: RegExp) => regx.test(current);
+    const addLength = (value: number) => sum + fontSize * value;
+
+    if (testChar(REGX_DEFS.LOWERCASE_BIG)) return addLength(0.85);
+    if (testChar(REGX_DEFS.LOWERCASE)) return addLength(0.5);
+    if (testChar(REGX_DEFS.SPACE)) return addLength(0.2);
+    if (testChar(REGX_DEFS.UPPERCASE_BIG)) return addLength(0.95);
+    if (testChar(REGX_DEFS.UPPERCASE)) return addLength(0.7);
+    if (testChar(REGX_DEFS.MARKS)) return addLength(0.3);
+    return addLength(0.5);
+  }, 0);
+
+  return calcLength;
 };
