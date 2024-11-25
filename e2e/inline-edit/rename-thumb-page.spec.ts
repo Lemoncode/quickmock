e2f420e2c349d3d50cf10e044460456a295c70a4
Fran Lopez <franciscolopto@gmail.com>
2024-11-25 15:24:15 +0100

    add functionality to rename thumbnail pages via direct edit and context menu

---
+++ e2e/inline-edit/rename-thumb-page.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test('rename-thumb-page-through-direct-edit-and-context-menu', async ({
+  page,
+}) => {
+  await page.goto('');
+
+  await page.getByText('Pages').click();
+  const thumb = page.getByText('Page 1');
+  await expect(thumb).toBeVisible();
+  await expect(thumb).toHaveText('Page 1');
+
+  await thumb.dblclick();
+  const input = page.getByRole('textbox');
+  const newTextContent = 'Change the name';
+  await input.fill(newTextContent);
+  await page.keyboard.press('Enter');
+  const renamedPage = page.getByText(newTextContent);
+  await expect(renamedPage).toBeVisible();
+
+  const siblingElement = page.getByText('Change the name');
+  const divThumb = siblingElement.locator('..');
+  await divThumb.click({ button: 'right' });
+  await page.getByText('Rename').click();
+
+  const secondNewTextContent = 'Other name';
+  await expect(input).toBeVisible();
+  await input.fill(secondNewTextContent);
+  await page.keyboard.press('Enter');
+  const secondRenamedPage = page.getByText(secondNewTextContent);
+  await expect(secondRenamedPage).toBeVisible();
+});
