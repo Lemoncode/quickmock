0331cc255ca93e7f65a6bcbd149bb54de3b5b346
Vero <veronicacamarzana@hotmail.com>
2024-11-27 17:55:26 +0100

    Updated color picker

--- src/pods/properties/components/color-picker/color-picker.component.tsx
+++ src/pods/properties/components/color-picker/color-picker.component.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import classes from './color-picker.component.module.css';
 import Chrome from '@uiw/react-color-chrome';
+import { hexToHsva } from '@uiw/color-convert';
 import { GithubPlacement } from '@uiw/react-color-github';
 
 interface Props {
@@ -8,13 +9,36 @@ interface Props {
   color: string;
   onChange: (color: string) => void;
 }
-
+const presetColors = [
+  '#00000000',
+  '#DD0000',
+  '#0022BB',
+  '#0088FF',
+  '#55EEFF',
+  '#006400',
+  '#00BB00',
+  '#99FF66',
+  '#FFFF00',
+  '#FF9900',
+  '#CC55FF',
+  '#FF88AA',
+  '#994400',
+  '#000000',
+  '#999999',
+  '#FFFFFF',
+];
 export const ColorPicker: React.FC<Props> = props => {
   const { label, color, onChange } = props;
   const [picker, setPicker] = useState(false);
+  const [hsva, setHsva] = useState(() => hexToHsva(color));
 
-  const togglePicker = () => setPicker(!picker);
+  const togglePicker = () => (picker ? setPicker(false) : setPicker(true));
 
+  const handlePresetColors = (newColor: string) => {
+    const hsvaColor = hexToHsva(newColor);
+    setHsva(hsvaColor);
+    onChange(newColor);
+  };
   return (
     <>
       <div className={classes.container}>
@@ -28,11 +52,27 @@ export const ColorPicker: React.FC<Props> = props => {
       {picker && (
         <div className={classes.popover}>
           <div className={classes.cover} onClick={togglePicker} />
-          <Chrome
-            placement={GithubPlacement.TopRight}
-            color={color}
-            onChange={color => onChange(color.hexa)}
-          />
+          <div className={classes.chromeContainer}>
+            <Chrome
+              style={{ boxShadow: 'none', border: 'none' }}
+              placement={GithubPlacement.TopRight}
+              color={hsva}
+              onChange={color => {
+                setHsva(color.hsva);
+                onChange(color.hexa);
+              }}
+            />
+            <div className={classes.colorPalette}>
+              {presetColors.map(presetColor => (
+                <div
+                  key={presetColor}
+                  className={classes.colorBox}
+                  style={{ backgroundColor: presetColor }}
+                  onClick={() => handlePresetColors(presetColor)}
+                ></div>
+              ))}
+            </div>
+          </div>
         </div>
       )}
     </>
