0930d3343b8f6584bd288673de5f9b2196be5b3c
Vero <veronicacamarzana@hotmail.com>
2024-11-29 14:56:01 +0100

    Mobile and Tablet resized

--- src/common/components/mock-components/front-containers/mobilephone-shape.tsx
+++ src/common/components/mock-components/front-containers/mobilephone-shape.tsx
@@ -10,8 +10,8 @@ const mobilePhoneShapeSizeRestrictions: ShapeSizeRestrictions = {
   minHeight: 150,
   maxWidth: 1000,
   maxHeight: 1000,
-  defaultWidth: 250,
-  defaultHeight: 450,
+  defaultWidth: 300,
+  defaultHeight: 560,
 };
 
 export const getMobilePhoneShapeSizeRestrictions = (): ShapeSizeRestrictions =>
