8ec4af736cc37916c66dd6d8b109822c622113b6
Braulio <braulio.diez@gmail.com>
2024-12-07 23:54:28 +0100

    removed img src

--- src/pods/properties/properties.model.ts
+++ src/pods/properties/properties.model.ts
@@ -13,7 +13,6 @@ export const multiSelectEnabledProperties: (keyof OtherProps)[] = [
   'checked',
   'icon',
   'iconSize',
-  'imageSrc',
   'imageBlackAndWhite',
   'progress',
   'borderRadius',
