9a4e099c682174244253774c6f9e84da6998ee3c
oleojake <oleojake@gmail.com>
2024-12-22 16:22:19 +0100

    #568 setCanvasSize from settings modal

--- src/pods/settings/components/canvas-size-settings.component.tsx
+++ src/pods/settings/components/canvas-size-settings.component.tsx
@@ -1,22 +1,45 @@
+import { CanvasSize } from '@/core/providers/canvas/canvas.model';
 import classes from './canvas-size-settings.component.module.css';
 
-export const CanvasSizeSettings = () => {
+interface CanvasSizeSettingsProps {
+  canvasSize: CanvasSize;
+  setNewCanvasSize: React.Dispatch<React.SetStateAction<CanvasSize>>;
+  newCanvasSize: CanvasSize;
+}
+
+export const CanvasSizeSettings: React.FC<CanvasSizeSettingsProps> = props => {
+  const { canvasSize, setNewCanvasSize, newCanvasSize } = props;
+
   return (
     <div className={classes.container}>
       <p className={classes.title}>Canvas Dimensions</p>
       <div className={classes.canvasSizeSettings}>
-        <p> Width:</p>
+        <p> Width (px):</p>
         <input
           type="text"
-          placeholder="Search icons..."
+          value={String(newCanvasSize.width)}
+          onChange={e =>
+            setNewCanvasSize({
+              ...newCanvasSize,
+              width: Number(e.target.value),
+            })
+          }
+          placeholder={String(canvasSize.width)}
           className={classes.searchInput}
         />
       </div>
       <div className={classes.canvasSizeSettings}>
-        <p>Height:</p>
+        <p>Height (px):</p>
         <input
           type="text"
-          placeholder="Search icons..."
+          value={String(newCanvasSize.height)}
+          onChange={e =>
+            setNewCanvasSize({
+              ...newCanvasSize,
+              height: Number(e.target.value),
+            })
+          }
+          placeholder={String(canvasSize.height)}
           className={classes.searchInput}
         />
       </div>
