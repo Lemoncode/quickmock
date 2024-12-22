c67fab5a6c2b882c92c2c9ab5dedc78dc89959a9
oleojake <oleojake@gmail.com>
2024-12-22 10:21:27 +0100

    #600 easy way with title attribute

--- src/pods/properties/components/zindex/zindex-button.component.tsx
+++ src/pods/properties/components/zindex/zindex-button.component.tsx
@@ -9,7 +9,12 @@ interface ButtonProps {
 export const ZIndexButton: React.FC<ButtonProps> = props => {
   const { onClick, Icon, label } = props;
   return (
-    <button onClick={onClick} className={classes.button} aria-label={label}>
+    <button
+      onClick={onClick}
+      className={classes.button}
+      aria-label={label}
+      title={label}
+    >
       <Icon />
     </button>
   );
