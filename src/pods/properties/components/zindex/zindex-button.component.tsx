3caa8d00d3a0cc235444c9c44f9fc494a340424f
oleojake <oleojake@gmail.com>
2024-12-24 16:02:32 +0100

    #600 tooltip component

--- src/pods/properties/components/zindex/zindex-button.component.tsx
+++ src/pods/properties/components/zindex/zindex-button.component.tsx
@@ -9,12 +9,7 @@ interface ButtonProps {
 export const ZIndexButton: React.FC<ButtonProps> = props => {
   const { onClick, Icon, label } = props;
   return (
-    <button
-      onClick={onClick}
-      className={classes.button}
-      aria-label={label}
-      title={label}
-    >
+    <button onClick={onClick} className={classes.button} aria-label={label}>
       <Icon />
     </button>
   );
