e2db175552504d06cc98abfed323b709531140dc
oleojake <oleojake@gmail.com>
2024-12-31 11:19:18 +0100

    #440 default values for rich-text

--- src/pods/canvas/model/inline-editable.model.ts
+++ src/pods/canvas/model/inline-editable.model.ts
@@ -98,7 +98,7 @@ const defaultTextValueMap: Partial<Record<ShapeType, string>> = {
   normaltext: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   smalltext: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   paragraph: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nSed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  richtext: `This is *bold* \n and _italic_ text`,
+  richtext: `This is a *bold text* example \\n an example of _italic text_ \\n and an example of ~underlined text~.`,
   table:
     'Name ^, Age ^v, Country v\nJohn Doe, 30, USA\nJane Smith, 25, UK\nLuis Gomez, 35, Argentina\n{*L,20R,30C}',
   modal:
