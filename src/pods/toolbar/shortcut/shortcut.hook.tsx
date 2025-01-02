1d0f1254e8919d116f5d35ba5a8efdc60dbb6bb4
oleojake <oleojake@gmail.com>
2025-01-02 10:38:56 +0100

    #635 allow shortcut.hook to accept [Alt] combinations

--- src/pods/toolbar/shortcut/shortcut.hook.tsx
+++ src/pods/toolbar/shortcut/shortcut.hook.tsx
@@ -1,6 +1,6 @@
+import { useEffect } from 'react';
 import { isMacOS } from '@/common/helpers/platform.helpers';
 import { useCanvasContext } from '@/core/providers';
-import { useEffect } from 'react';
 
 export interface ShortcutHookProps {
   targetKey: string[];
@@ -9,23 +9,26 @@ export interface ShortcutHookProps {
 
 export const useShortcut = ({ targetKey, callback }: ShortcutHookProps) => {
   const { isInlineEditing } = useCanvasContext();
+
   const handleKeyPress = (event: KeyboardEvent) => {
     if (isInlineEditing) {
       return;
     }
-    // TODO: later on this needs discussio about shortcut keys
-    // Right now enable CTRL+C, CTRL+V for windows, linux and mac
-    //const isAltKeyPressed = event.getModifierState('Alt');
-    //const isCtrlKeyPressed = event.getModifierState('Control');
+
     const isCtrlOrCmdPressed = event.ctrlKey || event.metaKey;
-
-    const ctrlKey = isMacOS() ? 'Meta' : 'Ctrl';
+    const isAltPressed = event.altKey;
     const pressedKey = event.key.toLowerCase();
+    const ctrlKey = isMacOS() ? 'Meta' : 'Ctrl';
 
-    if (
-      targetKey.includes(pressedKey) ||
-      (isCtrlOrCmdPressed && targetKey.includes(`${ctrlKey}+${pressedKey}`))
-    ) {
+    const pressedCombination = [
+      isCtrlOrCmdPressed ? ctrlKey : '',
+      isAltPressed ? 'Alt' : '',
+      pressedKey,
+    ]
+      .filter(Boolean)
+      .join('+');
+
+    if (targetKey.includes(pressedCombination)) {
       event.preventDefault();
       callback();
     }
