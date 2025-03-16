5b48082490682af8b84818aec33065c931345bd3
oleojake <oleojake@gmail.com>
2025-03-16 11:08:30 +0100

    #728-correct-low-wireframe-collection

--- src/pods/galleries/low-wireframe-gallery/low-wireframe-gallery.pod.tsx
+++ src/pods/galleries/low-wireframe-gallery/low-wireframe-gallery.pod.tsx
@@ -1,6 +1,6 @@
 import { GalleryComponent } from '@/common/components/gallery/gallery-component';
-import { mockTextCollection } from './low-wireframe-gallery-data';
+import { mockLowWireframeCollection } from './low-wireframe-gallery-data';
 
 export const LowWireframeGalleryPod = () => {
-  return <GalleryComponent itemCollection={mockTextCollection} />;
+  return <GalleryComponent itemCollection={mockLowWireframeCollection} />;
 };
