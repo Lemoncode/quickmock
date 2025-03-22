0e58a63d9d87d122530a39bbe28f77dbc4191b89
Braulio <braulio.diez@gmail.com>
2025-03-22 17:19:50 +0100

    minor update

--- src/core/model/index.ts
+++ src/core/model/index.ts
@@ -145,7 +145,7 @@ export const ShapeDisplayName: Record<ShapeType, string> = {
   videoconference: 'Videoconference',
   gauge: 'Gauge',
   imagePlaceholder: 'Image Placeholder',
-  horizontalLineLow: 'Horizontal Line',
+  horizontalLineLow: 'Horizontal Divider',
 };
 
 export type EditType = 'input' | 'textarea' | 'imageupload';
