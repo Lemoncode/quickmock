a64b554904fef1023c014d621e426ab1e161ac16
Monika Stefanova <monik.stefanova@gmail.com>
2025-03-27 19:46:40 +0100

    change ShapeDisplayName

--- src/core/model/index.ts
+++ src/core/model/index.ts
@@ -151,7 +151,7 @@ export const ShapeDisplayName: Record<ShapeType, string> = {
   horizontalLineLow: 'Horizontal Divider',
   verticalLineLow: 'Vertical Divider',
   ellipseLow: 'Ellipse',
-  rectangleLow: 'Rectangle Placeholder',
+  rectangleLow: 'Rectangle',
 };
 
 export type EditType = 'input' | 'textarea' | 'imageupload';
