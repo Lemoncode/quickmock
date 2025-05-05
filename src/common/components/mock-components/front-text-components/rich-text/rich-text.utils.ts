6187cb75c785636306434fb026d29113fcc9c363
oleojake <oleojake@gmail.com>
2025-05-05 20:46:07 +0200

    #761-regex-modified

--- src/common/components/mock-components/front-text-components/rich-text/rich-text.utils.ts
+++ src/common/components/mock-components/front-text-components/rich-text/rich-text.utils.ts
@@ -1,6 +1,7 @@
 export const parseTextWithFormatting = (text: string): string => {
   // Apply <br>
-  let parsedText = text.replace(/(?:\\n|<br>)/g, '<br>');
+  // Replaces line breaks (`\r\n`, `\r`, `\n`, `\\n`, or `<br>`) with `<br>` tags.
+  let parsedText = text.replace(/(\r\n|\r|\n|\\n|<br>)/g, '<br>');
 
   // Replace *text* <strong>text</strong>
   parsedText = parsedText.replace(/\*(.*?)\*/g, '<strong>$1</strong>');
