32738197b0f07aacdd9666572ca901beb82d9d93
Guste Gaubaite <219.guste@gmail.com>
2025-07-31 17:35:29 +0200

    feat: implement renderer for fab button shape

---
+++ src/pods/canvas/shape-renderer/simple-rich-components/fab-button.renderer.tsx
@@ -0,0 +1,32 @@
+import { ShapeRendererProps } from '../model';
+import { ShapeModel } from '@/core/model';
+import { FabButtonShape } from '@/common/components/mock-components/front-rich-components/fab-button/fab-button';
+
+export const renderFabButton = (
+  shape: ShapeModel,
+  shapeRenderedProps: ShapeRendererProps
+) => {
+  const { handleSelected, shapeRefs, handleDragEnd, handleTransform } =
+    shapeRenderedProps;
+
+  return (
+    <FabButtonShape
+      id={shape.id}
+      key={shape.id}
+      ref={shapeRefs.current[shape.id]}
+      x={shape.x}
+      y={shape.y}
+      name="shape"
+      width={shape.width}
+      height={shape.height}
+      draggable
+      typeOfTransformer={shape.typeOfTransformer}
+      onSelected={handleSelected}
+      onDragEnd={handleDragEnd(shape.id)}
+      onTransform={handleTransform}
+      onTransformEnd={handleTransform}
+      otherProps={shape.otherProps}
+      text={shape.text}
+    />
+  );
+};
