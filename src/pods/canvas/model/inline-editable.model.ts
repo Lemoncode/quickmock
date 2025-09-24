fffa03c149be665620a949b2c396f443313bd5ca
Guste Gaubaite <219.guste@gmail.com>
2025-09-24 15:39:21 +0200

    update(file-tree): update file tree default text to meet with dynamic symbol parsing

--- src/pods/canvas/model/inline-editable.model.ts
+++ src/pods/canvas/model/inline-editable.model.ts
@@ -117,7 +117,7 @@ const defaultTextValueMap: Partial<Record<ShapeType, string>> = {
   gauge: '10%',
   buttonBar: 'Button 1, Button 2, Button 3',
   tabsBar: 'Tab 1, Tab 2, Tab 3',
-  fileTree: 'Folder 1, Subfolder, File, Folder 2',
+  fileTree: '+ Folder 1, - Subfolder, * File, + Folder 2',
   link: 'Link',
   chip: 'Chip',
   timepickerinput: 'hh:mm',
