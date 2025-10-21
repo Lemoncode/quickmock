70fd33b3e2349a34fa1f1dd569cd8baf9299e94a
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-10-21 11:01:45 +0200

    align item to left

--- src/common/components/mock-components/front-rich-components/input-stepper.tsx
+++ src/common/components/mock-components/front-rich-components/input-stepper.tsx
@@ -66,9 +66,9 @@ export const InputWithStepper = forwardRef<any, ShapeProps>((props, ref) => {
 
       {/* Texto del input */}
       <Text
-        x={0}
+        x={INPUT_SHAPE.DEFAULT_PADDING}
         y={height / 2 - 8} // Centrar verticalmente
-        width={restrictedWidth}
+        width={width - INPUT_SHAPE.DEFAULT_PADDING * 2}
         text={text}
         fontFamily="Arial"
         fontSize={16}
